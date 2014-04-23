ae3143ef3fa80a047e489605d4680b78b7793e24
TheConnMan <bcconn2112@gmail.com>
2014-04-22 21:21:59 -0400

    Fixed build level hiding bug.

--- js/circles.js
+++ js/circles.js
@@ -66,6 +66,9 @@ $(document).ready(function() {
 		} else {
 			init(a[a.length - 1]);
 		}
+		if (a.length >= 10) {
+			$('#custom').show();
+		}
 	}
 	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 })
